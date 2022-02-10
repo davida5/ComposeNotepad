c48460c7942b809fdafc19617d968648d72e75d4
David Aleksanyan <aleksanyan.david@gmail.com>
2022-02-10 19:09:29 +0400

    First Navigation test completed

--- app/src/androidTest/java/com/anotherday/day17/navigation/NavigatorTest.kt
+++ app/src/androidTest/java/com/anotherday/day17/navigation/NavigatorTest.kt
@@ -1,14 +1,20 @@
 package com.anotherday.day17.navigation
 
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.printToLog
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.anotherday.day17.MainActivity
+import com.anotherday.day17.di.RoomModule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -23,14 +29,14 @@ class NavigatorTest {
 
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeRule = createAndroidComposeRule<MainActivity>()
     lateinit var navController: NavHostController
 
 
     @OptIn(ExperimentalMaterialApi::class)
     @Before
     fun setupNavigatorTest() {
-        composeTestRule.setContent {
+        composeRule.setContent {
             navController = rememberNavController()
             Navigator(navHostController = navController)
         }
@@ -38,11 +44,11 @@ class NavigatorTest {
 
     @Test
     fun testNavigationDefault() {
-        composeTestRule.onRoot().printToLog("currentLabelExists")
+        composeRule.onRoot().printToLog("currentLabelExists")
 
-//        composeTestRule
-//            .onNodeWithContentDescription("Navigator")
-//            .assertExists()
+        composeRule
+            .onNodeWithContentDescription("NotesList")
+            .assertIsDisplayed()
     }
 
 }
--- app/src/main/java/com/anotherday/day17/ui/NotesList.kt
+++ app/src/main/java/com/anotherday/day17/ui/NotesList.kt
@@ -5,31 +5,33 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.material.DismissDirection.*
+import androidx.compose.material.DismissDirection.EndToStart
+import androidx.compose.material.DismissDirection.StartToEnd
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Done
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.anotherday.day17.data.Note
+import com.anotherday.day17.navigation.NavDestinations
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
-import kotlin.math.roundToInt
 
 
 @ExperimentalMaterialApi
@@ -41,6 +43,7 @@ fun NotesList(
     notes: Flow<List<Note>>
 ) {
     Scaffold(
+        Modifier.semantics { contentDescription = NavDestinations.NotesList.route },
         topBar = {
             TopAppBar(
                 title = { Text("Notably") })
